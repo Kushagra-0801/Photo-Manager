ce44900f05fada51ff5727560fe36dd8048c38ad
Kushagra Gupta <kushgpt08@gmail.com>
2024-07-27 01:20:47 +0530

    basic interface for library

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -1,14 +1,99 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![allow(unused)]
+
+use std::error::Error;
+
+// #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+// pub struct ImageId(u32);
+
+pub type ImageId = u32;
+
+/// Example:
+/// let image_metadata = ImageMetadata {
+///     id: ImageId(42),
+///     available_formats: vec!["jpg", "png", "webp"],
+///     tags: vec!["abc", "def"]
+/// }
+pub struct ImageMetadata {
+    pub id: ImageId,
+    pub available_formats: Vec<String>,
+    pub tags: Vec<Tag>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct ImageData {
+    pub metadata: ImageMetadata,
+    pub data: Vec<u8>,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Tag(String);
+
+pub struct TagGraph {}
+
+/// A list of tags, to be searched by AND
+pub struct Query {
+    // list of tags
+    // - Tag name or Tag Id?
+    // - Can be searched by AND or OR
+    // Should I do a whole INCLUSION / EXCLUSION / multiple AND conditions joined by OR? Let's do it later
+    pub tags: Vec<Tag>,
+}
+
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
+#[allow(async_fn_in_trait)]
+pub trait Library {
+    async fn search_images(&self, query: Query) -> Result<Vec<ImageMetadata>>;
+
+    async fn add_image(&mut self, image: ImageData) -> Result<ImageMetadata>;
+    async fn find_image(&self, image_id: ImageId) -> Result<ImageMetadata>;
+    async fn update_image_tags(&mut self, image_id: ImageId, new_tags: Vec<String>) -> Result<()>;
+    async fn delete_image(&mut self, image_id: ImageId) -> Result<()>;
+
+    async fn add_tag(&mut self, tag: String) -> Result<Tag>;
+    async fn get_tag_list(&self) -> Result<TagGraph>;
+    async fn remove_tag(&mut self, tag: Tag) -> Result<()>;
+
+    async fn get_image_data(&self, image_id: ImageId, format: String) -> Result<Vec<u8>>;
+}
+
+impl Library for () {
+    async fn search_images(&self, query: Query) -> Result<Vec<ImageMetadata>> {
+        todo!()
     }
+
+    async fn add_image(&mut self, image: ImageData) -> Result<ImageMetadata> {
+        todo!()
+    }
+
+    async fn find_image(&self, image_id: ImageId) -> Result<ImageMetadata> {
+        todo!()
+    }
+
+    async fn update_image_tags(&mut self, image_id: ImageId, new_tags: Vec<String>) -> Result<()> {
+        todo!()
+    }
+
+    async fn delete_image(&mut self, image_id: ImageId) -> Result<()> {
+        todo!()
+    }
+
+    async fn add_tag(&mut self, tag: String) -> Result<Tag> {
+        todo!()
+    }
+
+    async fn get_tag_list(&self) -> Result<TagGraph> {
+        todo!()
+    }
+
+    async fn remove_tag(&mut self, tag: Tag) -> Result<()> {
+        todo!()
+    }
+
+    async fn get_image_data(&self, image_id: ImageId, format: String) -> Result<Vec<u8>> {
+        todo!()
+    }
+}
+
+pub fn format_to_mimetype(format: &str) -> &'static str {
+    todo!()
 }
--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -1,24 +1,76 @@
-use axum::{extract::Query, routing::get, Router};
-use library::add;
-use serde::Deserialize;
+#![allow(unused)]
 
-#[derive(Deserialize)]
-struct AddParams {
-    a: usize,
-    b: usize,
-}
+use axum::{
+    body::Body,
+    extract::{Multipart, Path, State},
+    http::{header, Response, StatusCode},
+    response::IntoResponse,
+    routing::{get, post},
+    Router,
+};
+use axum_macros::debug_handler;
+use library::{format_to_mimetype, ImageId, Library};
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
+    // let library = get_library();
+
+    let media_routes = Router::new()
+        .route("/:image_id", get(get_image).delete(remove_image))
+        .route("/", post(upload_image));
+
+    let tag_routes = Router::new();
+
     let app = Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
-        .route(
-            "/add",
-            get(|params: Query<AddParams>| async move { add(params.a, params.b).to_string() }),
-        );
+        .nest("/api/media", media_routes)
+        .nest("/api/tags", tag_routes)
+        .nest("/file", create_file_router())
+        .with_state(());
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:3001").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+/// Returns the metadata containing { id, thumbnail?, [{ format, size, dimensions, url }] }
+async fn get_image(Path(image_id): Path<String>) {
+    let _ = image_id;
+    todo!()
+}
+
+/// Removes from the library
+async fn remove_image(Path(image_id): Path<String>) {
+    let _ = image_id;
+    todo!()
+}
+
+/// Adds to the library
+async fn upload_image(file_data: Multipart) {
+    let _ = file_data;
+    todo!()
+}
+
+// fn get_library() -> impl Library {
+//     todo!()
+// }
+
+#[debug_handler]
+async fn get_image_data(Path(image): Path<String>, State(library): State<()>) -> impl IntoResponse {
+    let (id, format) = image.rsplit_once('.').unwrap();
+    let id = id.parse::<ImageId>().unwrap();
+    let image = library
+        .get_image_data(id, format.to_string())
+        .await
+        .unwrap();
+
+    (
+        StatusCode::OK,
+        [(header::CONTENT_TYPE, format_to_mimetype(format))],
+        image,
+    )
+}
+
+fn create_file_router() -> Router {
+    Router::new().route("/:image", get(get_image_data))
 }
